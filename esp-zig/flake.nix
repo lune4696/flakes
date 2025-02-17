0507f6aea05dfe31be750f34687e10d477826920
Lune4696 <lune.sqnm@gmail.com>
2025-02-17 10:02:39 +0900

    first commit

---
+++ esp-zig/flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "esp-zig development environment with nix flakes";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+    esp-overlay = {
+      url = "github:mirrexagon/nixpkgs-esp-dev";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    zig-shell = {
+      url = "github:lune4696/flakes/zig";
+    }; 
+  };
+
+  outputs = { self, nixpkgs, esp-overlay, zig-shell }: 
+    let
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in {
+      devShells = forAllSystems (system:
+       let
+        overlays = [ esp-overlay.overlays.default ];
+        pkgs = import nixpkgs { inherit system overlays; config.allowUnfree = true; };  
+        # allowUnfree = true: For Intel MKL, NVIDIA cuda
+       in {
+        default = pkgs.mkShell {
+          inputsFrom = [zig-shell.devShells.${system}.default esp-overlay.devShells.${system}.esp-idf-full];
+
+          packages = with pkgs; [ mkl ];
+
+          shellHook = ''
+            export PATH=${self}/bin:$PATH
+            export PS1="\n⛄\[\033[1;32m\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\$ \[\033[0m\]"
+            clear
+            echo -e "\nWelcome to zig-esp devShell!"
+          '';
+        };
+      });
+    };
+}
+
---
+++ zig/flake.nix
@@ -0,0 +1,100 @@
+{
+  description = "Zig development environment with Nix flakes";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+    zig-overlay = {
+      url = "github:mitchellh/zig-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, zig-overlay }: 
+    let
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+
+    in {
+     # for nix develop command
+      devShells = forAllSystems (system: 
+        let
+          overlays = [zig-overlay.overlays.default];
+          pkgs = import nixpkgs { inherit system overlays; };
+        in {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              # 基本ライブラリ
+              stdenv
+              pkgconf
+              # 線形演算ライブラリ
+              blas
+              lapack
+              # 言語別ライブラリ
+              zig
+              zls
+              # 依存パッケージ
+              raylib            # 描画と入出力全般を担当
+              alsa-lib          # オーディオ周りを使用する場合は無いとコンパイルできない(はず)
+              glfw              # linuxでのウィンドウ周り、無いとコンパイルできない 
+              libGL             # OpenGL関連、raylibはOpenGLベースなので無いとコンパイルできない
+              xorg.libX11       # X11絡みの全てに必要、無いとコンパイルできない
+              xorg.libXcursor   # 無いとコンパイルできない
+              xorg.libXinerama  # 無いとコンパイルできない
+              xorg.xinput       # 無いとコンパイルできない
+              xorg.libXrandr    # 無いとコンパイルできない
+              xorg.libXi        # raylib自体のコンパイルに必要
+              xorg.libXext      # raylib自体のコンパイルに必要
+              xorg.libXfixes    # raylib自体のコンパイルに必要
+              libxkbcommon      # raylib自体のコンパイルに必要
+              # ユーティリティ
+              cloc              # 行数カウンタ
+              fzf               # fuzzy finder
+              glxinfo           # GPU状態チェック
+              valgrind          # メモリリークチェック
+              lsof              # 同上
+              # GPGPU
+              rocmPackages.hipblas # rocblas/cublasをバックエンドに取るhip環境におけるblasライブラリ
+              rocmPackages.rocblas # roc版blas
+              rocmPackages.rocminfo
+              rocmPackages.rocm-smi
+            ];
+
+            NIX_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.pkgconf pkgs.raylib ];
+            LD_LIBRARY_PATH = "$(nix-build '<nixpkgs>' -A wayland)/lib";
+
+            # 環境変数設定 (オプション)
+            ZIG_CACHE_DIR = "./.zig-cache"; # Zigのビルドキャッシュディレクトリ
+            ZIG_GLOBAL_CACHE_DIR = "./.global_cache"; # グローバルキャッシュ
+
+            shellHook = ''
+              export PATH=${self}/bin:$PATH
+              export PS1="\n⛄\[\033[1;32m\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\$ \[\033[0m\]"
+              export SDL_GAMECONTROLLERCONFIG="045e,028e,Microsoft X-box 360 Pad"
+              clear
+              echo -e "\nWelcome to zig devShell!"
+            '';
+          };
+        }
+      );
+
+     # for nix build command
+      packages = forAllSystems (system: 
+        let
+          overlays = [zig-overlay.overlays.default];
+          pkgs = import nixpkgs { inherit system overlays;  };
+        in {
+          default = with pkgs; [
+            stdenv
+            blas
+            zig
+            zls
+          ];
+        });
+    }; 
+}
+
