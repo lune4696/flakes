ac87f8cf302659656485fbc19f573ff11e5e6262
Lune4696 <lune.sqnm@gmail.com>
2025-03-27 21:47:37 +0900

    zig is updated to 0.14

--- zig/flake.nix
+++ zig/flake.nix
@@ -3,13 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     zig-overlay = {
       url = "github:mitchellh/zig-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, zig-overlay }: 
+  outputs = { self, nixpkgs, nixpkgs-unstable, zig-overlay }: 
     let
       systems = [
         "aarch64-darwin"
@@ -25,49 +26,54 @@
         let
           overlays = [zig-overlay.overlays.default];
           pkgs = import nixpkgs { inherit system overlays; };
+          pkgs-unstable = import nixpkgs-unstable { inherit system overlays; };
+          stable-pkgs = with pkgs; [
+            # 基本ライブラリ
+            stdenv
+            pkgconf
+            # 線形演算ライブラリ
+            blas
+            lapack
+            # 依存パッケージ
+            raylib            # 描画と入出力全般を担当
+            # linux
+            alsa-lib          # オーディオ周りを使用する場合は無いとコンパイルできない(はず)
+            # OpenGL
+            glfw              # linuxでのウィンドウ周り、無いとコンパイルできない 
+            libGLU
+            libGL             # OpenGL関連、raylibはOpenGLベースなので無いとコンパイルできない
+            freeglut
+            # Xorg
+            xorg.libX11       # X11絡みの全てに必要、無いとコンパイルできない
+            #linuxPackages.vidia_x11 # nvidia独自のx11パッケ０ジング
+            xorg.libXcursor   # 無いとコンパイルできない
+            xorg.libXinerama  # 無いとコンパイルできない
+            xorg.xinput       # 無いとコンパイルできない
+            xorg.libXrandr    # 無いとコンパイルできない
+            xorg.libXi        # Ximage, raylib自体のコンパイルに必要
+            xorg.libXv        # Xvideo
+            xorg.libXext      # raylib自体のコンパイルに必要
+            xorg.libXfixes    # raylib自体のコンパイルに必要
+            libxkbcommon      # raylib自体のコンパイルに必要
+            # ユーティリティ
+            cloc              # 行数カウンタ
+            fzf               # fuzzy finder
+            glxinfo           # GPU状態チェック
+            helix             # editor
+            valgrind          # メモリリークチェック
+            # GPGPU
+            rocmPackages.rocblas # roc版blas
+            rocmPackages.rocminfo
+          ];
+          unstable-pkgs = with pkgs-unstable; [
+            # 言語別ライブラリ
+            zig
+            zls
+          ];
         in {
           default = pkgs.mkShell {
-            packages = with pkgs; [
-              # 基本ライブラリ
-              stdenv
-              pkgconf
-              # 線形演算ライブラリ
-              blas
-              lapack
-              # 言語別ライブラリ
-              zig
-              zls
-              # 依存パッケージ
-              raylib            # 描画と入出力全般を担当
-              # linux
-              alsa-lib          # オーディオ周りを使用する場合は無いとコンパイルできない(はず)
-              # OpenGL
-              glfw              # linuxでのウィンドウ周り、無いとコンパイルできない 
-              libGLU
-              libGL             # OpenGL関連、raylibはOpenGLベースなので無いとコンパイルできない
-              freeglut
-              # Xorg
-              xorg.libX11       # X11絡みの全てに必要、無いとコンパイルできない
-              #linuxPackages.vidia_x11 # nvidia独自のx11パッケ０ジング
-              xorg.libXcursor   # 無いとコンパイルできない
-              xorg.libXinerama  # 無いとコンパイルできない
-              xorg.xinput       # 無いとコンパイルできない
-              xorg.libXrandr    # 無いとコンパイルできない
-              xorg.libXi        # Ximage, raylib自体のコンパイルに必要
-              xorg.libXv        # Xvideo
-              xorg.libXext      # raylib自体のコンパイルに必要
-              xorg.libXfixes    # raylib自体のコンパイルに必要
-              libxkbcommon      # raylib自体のコンパイルに必要
-              # ユーティリティ
-              cloc              # 行数カウンタ
-              fzf               # fuzzy finder
-              glxinfo           # GPU状態チェック
-              helix             # editor
-              valgrind          # メモリリークチェック
-              # GPGPU
-              rocmPackages.rocblas # roc版blas
-              rocmPackages.rocminfo
-            ];
+
+            packages = stable-pkgs ++ unstable-pkgs;
 
             NIX_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.pkgconf pkgs.raylib ];
             LD_LIBRARY_PATH = "$(nix-build '<nixpkgs>' -A wayland)/lib";
